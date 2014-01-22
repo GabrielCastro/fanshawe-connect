58b6f9da97dbf90289a91349f7455300ca96d386
Gabriel Castro <dev@gabrielcastro.ca>
2014-01-22 01:29:20 -0500

    add Translucent status bar

--- FanshaweConnect/src/main/java/ca/GabrielCastro/fanshaweconnect/activities/BaseActivity.java
+++ FanshaweConnect/src/main/java/ca/GabrielCastro/fanshaweconnect/activities/BaseActivity.java
@@ -20,13 +20,37 @@
 package ca.GabrielCastro.fanshaweconnect.activities;
 
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
 import com.google.analytics.tracking.android.EasyTracker;
+import com.readystatesoftware.systembartint.SystemBarTintManager;
 
-import ca.GabrielCastro.fanshaweconnect.util.AnalyticsWrapper;
+import ca.GabrielCastro.fanshaweconnect.R;
 
 public class BaseActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        int color = getResources().getColor(R.color.action_bar_red);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            SystemBarTintManager tintManager = new SystemBarTintManager(this);
+            tintManager.setStatusBarTintEnabled(true);
+            tintManager.setTintColor(color);
+        }
+
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(color));
+        Drawable icon = getResources().getDrawable(R.drawable.ic_launcher);
+        icon.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
+        getSupportActionBar().setIcon(icon);
+
+    }
 
     @Override
     protected void onStart() {
